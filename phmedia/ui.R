e1e6a5356a86292ef7f6c76183231ff6ac137959
fhernanb <fhernanb@gmail.com>
2016-12-05 21:22:27 -0500

    cambios menores

--- phmedia/ui.R
+++ phmedia/ui.R
@@ -1,11 +1,11 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("Prueba de hipótesis para la media", "Flowserve"),
+  headerPanel("Prueba de hipotesis para la media", "Flowserve"),
   sidebarPanel(
-    h5('Esta aplicacion sirve para realizar la prueba de hipótesis para la media de
-       una variable cuantitativa. Ingrese la información solicitada abajo.'),
+    h5('Esta aplicacion sirve para realizar la prueba de hipotesis para la media de
+       una variable cuantitativa. Ingrese la informacion solicitada abajo.'),
     br(),
-    fileInput('file1', 'Use el boton siguiente para cargar la base de datos.',
+    fileInput('file1', 'Use el boton siguiente para cargar su base de datos.',
               accept = c(
                 'text/csv',
                 'text/comma-separated-values',
@@ -20,7 +20,7 @@ shinyUI(pageWithSidebar(
                  c(Tab='\t', Comma=',', Semicolon=';' )
     ),
     tags$hr(),
-    selectInput("product", "Seleccione la variable de interés de la base de datos",""),
+    selectInput("product", "Seleccione la variable de interes de la base de datos",""),
     tags$hr(),
     
     numericInput(inputId='alfa', label='Elija el nivel de significancia', 
@@ -31,14 +31,14 @@ shinyUI(pageWithSidebar(
                  value=0.05, min=0.01, 
                  step=0.01, max=0.10),
     
-    selectInput("h0", label = h5("Elija la hipótesis alterna"), 
-                choices = list("Choice 1" = 1, 
-                               "Choice 2" = 2,
-                               "Choice 3" = 3),
-                selected = 1),
+    selectInput("h0", label = h5("Elija la hipotesis alterna"), 
+                choices = list("<" = 1, 
+                               "diferente" = 2,
+                               ">" = 3),
+                selected = 2),
     
-    img(src="udea.png", height = 72, width = 64),
-    img(src="logo.png", height = 56, width = 136),
+    img(src="logo.png", height = 60, width = 150),
+    br(),
     
     tags$a(href="https://srunal.wordpress.com/", "https://srunal.wordpress.com/")
 
