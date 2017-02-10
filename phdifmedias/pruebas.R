65ade02c498ef96c43c69d7a602bd412dfaf5185
fhernanb <fhernanb@unal.edu.co>
2017-02-10 10:02:03 -0500

    mejoras

--- phdifmedias/pruebas.R
+++ phdifmedias/pruebas.R
@@ -1,4 +1,6 @@
 
+# Para generar base de datos DIFERENCIA DE MEDIAS -------------------------
+
 dt <- data.frame(y=c(rnorm(n=100, mean=170, sd=5),
                      rnorm(n=150, mean=173, sd=5)),
                  group=rep(c('Grupo 1', 'Grupo 2'), times=c(100, 150)))
@@ -13,3 +15,13 @@ dt <- data.frame(y=c(rnorm(n=100, mean=65, sd=10),
 
 
 write(x=t(dt), file='unequal_var_data.txt', ncolumns=2, sep='\t')
+
+
+
+# Para generar base de datos MEDIAS ---------------------------------------
+
+dt <- data.frame(y=rnorm(n=100, mean=60, sd=5),
+                 x=rnorm(n=100, mean=170, sd=15))
+
+write(x=t(dt), file='means_data.txt', ncolumns=2, sep='\t')
+
--- phmedia/server.R
+++ phmedia/server.R
@@ -3,30 +3,37 @@ shinyServer(function(input,output,session){
   
   observe({
     inFile <- input$file1
-    if(is.null(inFile)) return(NULL)
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    if(is.null(inFile)) 
+      dt <- read.table('means_data.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
     updateSelectInput(session, "variable", choices = names(dt))
   })
   
   output$summary <- renderTable({
     inFile <- input$file1
-    if(is.null(inFile)) return(NULL)
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    if(is.null(inFile)) 
+      dt <- read.table('means_data.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
     dt
   })
   
   output$statistic <- renderTable({
     inFile <- input$file1
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
-    y <- na.omit(dt[, input$variable])
+    if(is.null(inFile)) 
+      dt <- read.table('means_data.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    y <- na.omit(dt[, input$variable])  # Para sacar los NA de la variable
     res <- data.frame(Media=mean(y), Varianza=var(y), n=length(y))
+    res
   })
   
   output$distPlot <- renderPlot({
     inFile <- input$file1
-    par(mfrow=c(1, 2))
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
-    y <- na.omit(dt[, input$variable])
+    if(is.null(inFile)) 
+      dt <- read.table('means_data.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    par(mfrow=c(1, 2), bg='gray98')
+    y <- na.omit(dt[, input$variable])  # Para sacar los NA de la variable
     hist(y, col='deepskyblue3', freq=F, las=1,
          xlab=as.character(input$variable), main='Histograma y densidad',
          ylab='Densidad')
@@ -42,22 +49,28 @@ shinyServer(function(input,output,session){
   
   output$resul1 <- renderText({
     inFile <- input$file1
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
-    y <- na.omit(dt[, input$variable])
+    if(is.null(inFile)) 
+      dt <- read.table('means_data.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    y <- na.omit(dt[, input$variable])  # Para sacar los NA de la variable
     ph <- t.test(x=y, alternative=input$h0, mu=input$mu0, conf.level=input$alfa)
-    conclusion <- ifelse(ph$p.value < 0.05, 'se rechaza.', 'no se rechaza.')
+    conclusion <- ifelse(ph$p.value < 0.05, 'se rechaza', 'no se rechaza')
     paste0('El estadístico de prueba fue to=', round(ph$statistic, 2),
-           ' con un valor P de ', round(ph$p.value, 2), ', por lo tanto se concluye
-           que basados en la evidencia muestral la hipótesis nula ', conclusion)
+           ' con un valor P de ', round(ph$statistic, 4), ', por lo tanto se concluye
+           que basados en la evidencia muestral la hipótesis nula ', conclusion,
+           ' (nivel de significancia 5%).')
   })
   
   
    output$resul2 <- renderText({
-    inFile <- input$file1
-    dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
-    y <- na.omit(dt[, input$variable])
+     inFile <- input$file1
+     if(is.null(inFile)) 
+       dt <- read.table('means_data.txt', header=T, sep='\t')
+     else dt <- read.csv(inFile$datapath, header=input$header, sep=input$sep)
+    y <- na.omit(dt[, input$variable])  # Para sacar los NA de la variable
     ph <- t.test(x=y, alternative=input$h0, mu=input$mu0, conf.level=input$alfa)
-    intervalo <- paste("(", ph$conf.int[1], ", ", ph$conf.int[2], ").", sep='')
+    intervalo <- paste("(", round(ph$conf.int[1], digits=4), ", ",
+                       round(ph$conf.int[2], digits=4), ").", sep='')
     paste0('El intervalo de confianza del ', 100*input$alfa,
            '% para la media poblacional es ', intervalo)
   })
