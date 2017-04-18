35edd3fdf8b78b1ecc20a4e2c86075ff1905e88a
fhernanb <fhernanb@unal.edu.co>
2017-04-18 12:32:02 -0500

    se incluyo

---
+++ goodFit/server.R
@@ -0,0 +1,87 @@
+library(shiny)
+require(gamlss)
+
+shinyServer(function(input,output,session){
+  observe({
+    inFile <- input$file1
+    if(is.null(inFile)) 
+      dt <- read.table('datos.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, 
+                        sep=input$sep)
+    updateSelectInput(session, "product",
+                      choices = names(dt))
+    updateSelectInput(session, "familia")
+  })
+  require(gamlss)
+  f1<-function(k,f,p){
+    m <- fitDist(dt[,p], type=f, k=k)
+    par(mfrow=c(2, 2))
+    for(i in 1:4){
+      denst <- density(dt[,p])
+      res <- histDist(dt[,p], family=names(m$fits)[i],
+                      main=names(m$fits)[i],
+                      xlab=p,
+                      line.wd=3,
+                      line.ty=1,
+                      line.col='dodgerblue2',
+                      ylim=c(0, (1.3 * max(denst$y))))
+      param <- c('mu', 'sigma', 'nu', 'tau') 
+      np <- length(res$parameters)
+      fun1 <- function(x) eval(parse(text=x))
+      hat.param <- sapply(as.list(paste('res$',
+                                        param[1:np], 
+                                        sep='')),fun1)
+      hat.param <- round(hat.param, digits=2)
+      txt <- paste('hat(', param[1:np], ')==', hat.param, sep='')
+      txt <- paste(txt, collapse=', ')
+      legend('topright', bty='n',
+             legend=eval(parse(text=paste('expression(', txt, ')'))))
+    }
+  }
+  output$distPlot <- renderPlot({
+    inFile <- input$file1
+    if(is.null(inFile))
+      dt <- read.table('datos.txt', header=T, sep='\t')
+    else dt <- read.csv(inFile$datapath, header=input$header, 
+                        sep=input$sep)
+
+    f1 <- function(k, f, p){
+      m <- fitDist(dt[, p], type=f, k=k)
+      par(mfrow=c(2, 2))
+      for(i in 1:4){
+        denst <- density(dt[, p])
+        res <- histDist(dt[,p], family=names(m$fits)[i],
+                        main=names(m$fits)[i],
+                        xlab=p, las=1,
+                        line.wd=3,
+                        line.ty=1,
+                        line.col='dodgerblue2',
+                        ylim=c(0, (3 * max(denst$y))))
+        param <- c('mu', 'sigma', 'nu', 'tau') 
+        np <- length(res$parameters)
+        fun1 <- function(x) eval(parse(text=x))
+        hat.param <- sapply(as.list(paste('res$',
+                                          param[1:np], 
+                                          sep='')),fun1)
+        hat.param <- round(hat.param, digits=2)
+        txt <- paste('hat(', param[1:np], ')==', hat.param, sep='')
+        txt <- paste(txt, collapse=', ')
+        legend('topright', bty='n',
+               legend=eval(parse(text=paste('expression(', txt, ')'))))
+      }
+    }
+    f1(input$k,input$familia,input$product)
+  })
+  
+  output$descarga1<-downloadHandler(
+    filename=function(){
+      paste("grafica","png",sep=".")},content=function(file){
+        png(file)
+        print(f1(input$k,input$familia,input$product))
+        dev.off()
+      }
+  )
+  output$markdown <- renderUI({
+    HTML(markdown::markdownToHTML(knit('Teoria.Rmd', quiet = TRUE)))
+  })
+})
---
+++ goodFit/ui.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(knitr)
+
+shinyUI(pageWithSidebar(
+  headerPanel( "Explorando la distribución que mejor se ajusta a una variable"),
+  sidebarPanel(h3("Objetivo:"),
+h5('Esta aplicación sirve para mostrar
+las mejores cuatro distribuciones 
+que se ajustan a una variable elegida de 
+una base de datos. Se muestra una gráfica 
+con una base predeterminada inicialmente 
+(Siendo el mejor ajuste BCPEo, seguido por 
+la Exp y asi respectivamente). A continuación 
+suba su base de interes e indique con exactitud el tipo de separación de su base.'),
+br(),
+fileInput(inputId='file1', label='Use el boton siguiente para
+          cargar la base de datos.',
+          accept = c(
+            'text/csv',
+            'text/comma-separated-values',
+            'text/tab-separated-values',
+            'text/plain',
+            '.csv',
+            '.tsv'
+            )),
+
+checkboxInput('header', label='Tiene encabezado la base
+              de datos?', value=TRUE),
+
+selectInput(inputId="sep", label="Cual es la separacion de sus datos?",
+             choices=list(Tabulacion='\t',Coma=',',PuntoyComa=';'),
+             selected=';'),
+
+tags$hr(),
+
+selectInput("product",label="Seleccione la variable 
+            de la base de datos",choices=""),
+
+selectInput("familia", "Seleccione la familia de distribuciones 
+            a analizar",choice=list("Reales"="realAll",
+                            "Reales Lineales"="realline",
+                            "Reales Positivas"="realplus",
+                            "Reales de 0 a 1"="real0to1",
+                            "Conteos"="counts",
+                            "Binomiales"="binom")),
+
+sliderInput("k","Ingrese una penalización K por cantidad 
+            de parametros",
+            min=1,
+            max=10,
+            value=4,
+            step=1,
+            animate=TRUE),
+img(src="medellin.png", height = 80, width = 200),
+br(),
+br(),
+p("App creada por el Semillero de R de la Universidad Nacional 
+      de Colombia:"),
+tags$a(href="https://srunal.wordpress.com/", 
+           "https://srunal.wordpress.com/")),
+mainPanel(
+  tabsetPanel(type ="pills",
+tabPanel("Ajuste",h4('A continuacion el ajuste para 
+la variable seleccionada por el usuario'),
+         plotOutput("distPlot",width="800px",height="600px"),
+         downloadButton(outputId="descarga1",'Descargar en png la gráfica de su base')),
+tabPanel("Teoria",uiOutput('markdown'))
+    )
+  )
+ )
+)
+ 
+
