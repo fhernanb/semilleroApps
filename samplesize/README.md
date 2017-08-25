215759c64320f450db4283a6ba6af554ed03c94c
fhernanb <fhernanb@unal.edu.co>
2017-08-25 13:11:46 -0500

    mejorado

--- samplesize/README.md
+++ samplesize/README.md
@@ -1,1 +1,1 @@
-Esta aplicacion sirve para obtener el tamano de muestra para diferentes niveles de confianza.
+Esta aplicación sirve para obtener el tamaño de muestra para diferentes niveles de confianza.
--- samplesize/ui.R
+++ samplesize/ui.R
@@ -1,8 +1,8 @@
 shinyUI(fluidPage(
-  titlePanel(h1("Tamanos de muestra",align="center", style="color:steelblue")),
-  h4(p("Esta aplicacion entrega los tamanos de muestra para
-        diferentes niveles de confianza cuando el interes es
-        estimar una proporcion o una media."), align="center"),
+  titlePanel(h1("Tamaños de muestra",align="center", style="color:steelblue")),
+  h4(p("Esta aplicación entrega los tamaños de muestra para
+        diferentes niveles de confianza cuando el interés es
+        estimar una proporción o una media."), align="left"),
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId="tipo",
@@ -15,20 +15,20 @@ shinyUI(fluidPage(
       conditionalPanel(condition="input.tipo=='Proporcion'",
                        
                        selectInput(inputId="Ncp",
-                                    label="Conoce usted el tamano N de la poblacion?",
+                                    label="Conoce usted el tamaño N de la población?",
                                     choices=c('Si', 'No'),
                                     selected="No"),
                        
                        conditionalPanel("input.Ncp=='Si'",
                        
                          numericInput(inputId="Nprop",
-                                     label="Ingrese el tamaño de la poblacion",
+                                     label="Ingrese el tamaño de la población",
                                      min=5,
                                      value=500,
                                      step=1) ),
                        
                        sliderInput(inputId="p",
-                                    label="Ingrese una estimacion preliminar para p, 
+                                    label="Ingrese una estimación preliminar para p, 
                                     caso no tenga idea use 0.5.",
                                     min=0.01,
                                     max=0.99,
@@ -46,20 +46,20 @@ shinyUI(fluidPage(
       conditionalPanel(condition="input.tipo=='Media'",
                        
                        selectInput(inputId="Ncm",
-                                   label="Conoce usted el tamano N de la poblacion?",
+                                   label="Conoce usted el tamaño N de la población?",
                                    choices=c('Si', 'No'),
                                    selected="No"),
                        
                        conditionalPanel("input.Ncm=='Si'",
                                         
                          numericInput(inputId="Nmean",
-                                      label="Ingrese el tamano de la poblacion N",
+                                      label="Ingrese el tamaño de la población N",
                                       min=2,
                                       value=30,
                                       step=1) ),
                        
                        numericInput(inputId="sigma",
-                                    label=HTML("Ingrese una estimacion de &sigma;:"),
+                                    label=HTML("Ingrese una estimación de &sigma;:"),
                                     min=0.01,
                                     max=1000,
                                     value=12,
@@ -69,10 +69,7 @@ shinyUI(fluidPage(
                                     label=HTML("Ingrese el error admisible &epsilon;:"),
                                     min=0.01,
                                     value=5,
-                                    step=0.1)),
-
-img(src="logo.png", height = 56, width = 136),
-img(src="udea.png", height = 72, width = 64)
+                                    step=0.1))
 
     ),
 
