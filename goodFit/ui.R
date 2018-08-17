b6300b219c55dec1781af9865147912817539dd9
fhernanb <fhernanb@unal.edu.co>
2018-08-17 15:27:50 -0500

    pasando a ingles el contenido

--- goodFit/ui.R
+++ goodFit/ui.R
@@ -2,63 +2,62 @@ library(shiny)
 library(knitr)
 
 shinyUI(pageWithSidebar(
-  headerPanel( "Explorando la distribución que mejor se ajusta a una variable"),
+  headerPanel("Exploring the distribution that fits better for a variable"),
   
 # Here the sidebarpanel
   sidebarPanel(
-h6("Esta aplicación sirve para identificar
-    las cuatro distribuciones 
-    que mejor se ajustan a una variable de 
-    una base de datos ingresada por el usuario."),
-h6('Como ejemplo se muestran los resultados obtenidos para 
-   la variable speed de la base de datos cars de R.'),
-h4('Para usar la aplicación haga lo siguiente:'),
-h6('1. Suba su base,'),
-h6('2. Indique el tipo de separación de su base,'),
-h6('3. Seleccione la variable de interés.'),
-fileInput(inputId='file1', label='Use el botón siguiente para
-          cargar la base de datos.',
-          accept = c(
-            'text/csv',
-            'text/comma-separated-values',
-            'text/tab-separated-values',
-            'text/plain',
-            '.csv',
-            '.tsv'
-            )),
-checkboxInput('header', label='¿Tiene encabezado la base
-              de datos?', value=TRUE),
-selectInput(inputId="sep", label="¿Cual es la separación de sus datos?",
-             choices=list('tab'='\t', ','=',', ';'=';', 'space'=' '),
-             selected=';'),
-selectInput("response", label="Seleccione la variable 
-            de la base de datos", choices=""),
-selectInput("familia", "Seleccione la familia a la cual pertenece 
-            la variable de interés:", 
-            choice=list("Reales positivos" = "realplus",
-                        "Reales" = "realAll",
-                        "Reales entre 0 y 1" = "real0to1",
-                        "Conteos" = "counts",
-                        "Binomiales" = "binom")),
-sliderInput("k", 'Ingrese una penalización \\( k \\) para 
-                 penalizar el exceso de parámetros 
-                 en el calculo del \\(GAIC=-2 \\times logLik + k \\times df\\)
-                 donde \\( df \\) es número de parametros 
-                 del modelo.',
-            min=2,
-            max=20,
-            value=2,
-            step=1,
-            animate=TRUE),
-radioButtons(inputId = "type_plot", 
-             label = "Select the file type to download the plot", 
-             choices = list("pdf", "png")),
-br(),
-p("This app was created by Semillero de R at Universidad Nacional 
-      de Colombia:"),
-img(src="https://fhernanb.github.io/docs/logo_unal_negro.png",
-    height = 56, width = 140),
-tags$a(href="https://srunal.github.io", "https://srunal.github.io")
+    h6("This app can be used to identify the best four
+        distributions that fit better for a variable. The user
+        can provide the dataset using the buttons below."),
+    h6("As an example the app uses the speed variable from
+        the dataset cars."),
+    h4("To use the app follow the next steps:"),
+    h6('1. Suba su base,'),
+    h6('2. Indique el tipo de separación de su base,'),
+    h6('3. Seleccione la variable de interés.'),
+    fileInput(inputId='file1', label='Use el botón siguiente para
+              cargar la base de datos.',
+              accept = c(
+                'text/csv',
+                'text/comma-separated-values',
+                'text/tab-separated-values',
+                'text/plain',
+                '.csv',
+                '.tsv'
+                )),
+    checkboxInput('header', label='¿Tiene encabezado la base
+                  de datos?', value=TRUE),
+    selectInput(inputId="sep", label="¿Cual es la separación de sus datos?",
+                 choices=list('tab'='\t', ','=',', ';'=';', 'space'=' '),
+                 selected=';'),
+    selectInput("response", label="Seleccione la variable 
+                de la base de datos", choices=""),
+    selectInput("familia", "Seleccione la familia a la cual pertenece 
+                la variable de interés:", 
+                choice=list("Reales positivos" = "realplus",
+                            "Reales" = "realAll",
+                            "Reales entre 0 y 1" = "real0to1",
+                            "Conteos" = "counts",
+                            "Binomiales" = "binom")),
+    sliderInput("k", 'Ingrese una penalización \\( k \\) para 
+                     penalizar el exceso de parámetros 
+                     en el calculo del \\(GAIC=-2 \\times logLik + k \\times df\\)
+                     donde \\( df \\) es número de parametros 
+                     del modelo.',
+                min=2,
+                max=20,
+                value=2,
+                step=1,
+                animate=TRUE),
+    radioButtons(inputId = "type_plot", 
+                 label = "Select the file type to download the plot", 
+                 choices = list("pdf", "png")),
+    br(),
+    p("This app was created by Semillero de R at Universidad Nacional 
+          de Colombia:"),
+    img(src="https://fhernanb.github.io/docs/logo_unal_negro.png",
+        height = 56, width = 140),
+    tags$a(href="https://srunal.github.io", "https://srunal.github.io")
 ),
 
 # Here the panels
