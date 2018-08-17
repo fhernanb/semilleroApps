d399a58c3d25186d65b3976c19cf1aec805c3ab6
fhernanb <fhernanb@unal.edu.co>
2018-08-17 15:58:20 -0500

    cambiando a ingles

--- goodFit/ui.R
+++ goodFit/ui.R
@@ -10,40 +10,37 @@ shinyUI(pageWithSidebar(
         distributions that fit better for a single variable. The user
         can provide the dataset using the buttons below."),
     h6("As an example the app uses the speed variable from
-        the dataset cars."),
+        the cars dataset."),
     h4("To use the app follow the next steps:"),
-    h6('1. Upload the file,'),
-    h6('2. Select the separator character,'),
-    h6('3. Select the variable.'),
-    fileInput(inputId='file1', label='Use el botón siguiente para
-              cargar la base de datos.',
+    h6("1. Upload the file,"),
+    h6("2. Select the separator character,"),
+    h6("3. Select the variable."),
+    fileInput(inputId="file1", label="Use the next button to load your file.",
               accept = c(
-                'text/csv',
-                'text/comma-separated-values',
-                'text/tab-separated-values',
-                'text/plain',
-                '.csv',
-                '.tsv'
+                "text/csv",
+                "text/comma-separated-values",
+                "text/tab-separated-values",
+                "text/plain",
+                ".csv",
+                ".tsv"
                 )),
-    checkboxInput('header', label='¿Tiene encabezado la base
-                  de datos?', value=TRUE),
-    selectInput(inputId="sep", label="¿Cual es la separación de sus datos?",
-                 choices=list('tab'='\t', ','=',', ';'=';', 'space'=' '),
-                 selected=';'),
-    selectInput("response", label="Seleccione la variable 
-                de la base de datos", choices=""),
-    selectInput("familia", "Seleccione la familia a la cual pertenece 
-                la variable de interés:", 
-                choice=list("Reales positivos" = "realplus",
-                            "Reales" = "realAll",
-                            "Reales entre 0 y 1" = "real0to1",
-                            "Conteos" = "counts",
-                            "Binomiales" = "binom")),
-    sliderInput("k", 'Ingrese una penalización \\( k \\) para 
-                     penalizar el exceso de parámetros 
-                     en el calculo del \\(GAIC=-2 \\times logLik + k \\times df\\)
-                     donde \\( df \\) es número de parametros 
-                     del modelo.',
+    checkboxInput("header", label="Does your file contain variable names
+                  in the first line?", value=TRUE),
+    selectInput(inputId="sep", label="What is the separator character in your file?",
+                 choices=list("tab"="\t", ","=",", ";"=";", "space"=" "),
+                 selected=";"),
+    selectInput("response", label="Select the variable.", choices=""),
+    selectInput("familia", "Select the family for the response variable:", 
+                choice=list("Real plus" = "realplus",
+                            "Real" = "realAll",
+                            "Real between entre 0 y 1" = "real0to1",
+                            "Counts" = "counts",
+                            "Binomial" = "binom")),
+    sliderInput("k", "Choose the number \\( k \\) to penalize for 
+                     the excess number of parameters
+                     in \\(GAIC=-2 \\times logLik + k \\times df\\).
+                     The value \\( df \\) is the number of 
+                     distribution parameters",
                 min=2,
                 max=20,
                 value=2,
@@ -64,21 +61,21 @@ shinyUI(pageWithSidebar(
 mainPanel(
   tabsetPanel(type ="pills",
               tabPanel("Selected distributions I",
-                       h4('A continuación se presenta el histograma para 
+                       h4("A continuación se presenta el histograma para 
                           la variable de interés
-                          con la curva de densidad para cada distribución.'),
+                          con la curva de densidad para cada distribución."),
                        plotOutput("distPlot1", width="700px", height="600px"),
                        downloadButton(outputId = "downplot1", label = "Download the plot")
               ),
               tabPanel("Selected distributions II",
-                       h4('A continuación se presenta el histograma para 
+                       h4("A continuación se presenta el histograma para 
                           la variable de interés
                           con la curva de densidad para cada distribución y
-                          el qqplot asociado.'),
+                          el qqplot asociado."),
                        plotOutput("distPlot2", width="700px", height="800px"),
                        downloadButton(outputId = "downplot2", label = "Download the plot")
               ),
-              tabPanel('Data', uiOutput('data_table')),
+              tabPanel("Data", uiOutput("data_table")),
               tabPanel("Goodness of fit test"),
               tabPanel("Theory", includeHTML("include.html"))
     )
