77b74dc41c0b6f858b4e9f345b867fb2f9d2786f
fhernanb <fhernanb@unal.edu.co>
2022-04-05 16:10:22 -0500

    nueva app

---
+++ glm_dists/server.R
@@ -0,0 +1,58 @@
+library(shiny)
+
+source("www/auxiliar_functions.R")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+      
+      if (input$Distribucion == "Normal")
+          curve(dnorm(x, mean=input$mu_normal, 
+                      sd=input$sigma),
+                ylab="Density", xlab="X", las=1,
+                xlim=c(input$min_normal, input$max_normal), lwd=3, col="indianred1")
+      
+      if (input$Distribucion == "Gamma")
+        curve(dgamma_glm(x, mu=input$mu_gamma, 
+                         phi=input$phi_gamma),
+              ylab="Density", xlab="X", las=1,
+              xlim=c(input$min_gamma, input$max_gamma), lwd=3, col="dodgerblue2")
+      
+      if (input$Distribucion == "Inv. gaussian")
+         curve(dinvgaus_glm(x, mu=input$mu_invgaus, 
+                            phi=input$phi_invgaus),
+              ylab="Density", xlab="X", las=1,
+              xlim=c(input$min_invgaus, input$max_invgaus), lwd=3, col="darkslategray3")
+      
+      if (input$Distribucion == "Binomial") {
+        xs <- seq(from=0, to=input$m)
+        ys <- dbinom(x=xs, size=input$m, prob=input$mu_binom)
+        plot(x=xs, y=ys, type='p', xlab='X', ylab='Probability', 
+             col='mediumpurple1', lwd=2, las=1)
+        segments(xs, 0, xs, ys, col="mediumpurple1", lwd=3)
+      }
+      
+      if (input$Distribucion == "Poisson") {
+        xs <- seq(from=0, to=input$max_pois)
+        ys <- dpois(x=xs, lambda=input$mu_pois)
+        plot(x=xs, y=ys, type='p', xlab='X', ylab='Probability', 
+             col='lightsalmon2', lwd=2, las=1)
+        segments(xs, 0, xs, ys, col="lightsalmon2", lwd=3)
+      }
+      
+      if (input$Distribucion == "Negative binomial") {
+        xs <- seq(from=0, to=input$max_negbin)
+        ys <- dnbinom(x=xs, mu=input$mu_negbin, size=input$k)
+        plot(x=xs, y=ys, type='p', xlab='X', ylab='Probability', 
+             col='olivedrab3', lwd=2, las=1)
+        segments(xs, 0, xs, ys, col="olivedrab3", lwd=3)
+      }
+
+      
+    }, res = 96)
+
+})
+
+
+  
---
+++ glm_dists/ui.R
@@ -0,0 +1,146 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("glm distributions"),
+    
+    h4("Select the glm distribution and then fixed
+       the parameters to obtain the probability distribution."),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+          
+          selectInput("Distribucion",
+                      "What distribution?",
+                      choices=c("Normal", "Gamma", "Inv. gaussian", 
+                                "Binomial",
+                                "Poisson", "Negative binomial"),
+                      selected="Normal"),
+          
+          conditionalPanel(condition="input.Distribucion=='Normal'",
+                           numericInput(inputId="mu_normal",
+                                        label=HTML("Select &mu;:"),
+                                        value="0.5",
+                                        step=0.01),
+                           
+                           numericInput(inputId="sigma",
+                                        label=HTML("Select &sigma;:"),
+                                        min=0.01,
+                                        value="1.2",
+                                        step=0.01),
+                           
+                           numericInput(inputId="min_normal",
+                                        label="Select the minimun value for xlim",
+                                        value="-1", step=1),
+                           numericInput(inputId="max_normal",
+                                        label="Select the maximum value for xlim",
+                                        value="3", step=1)
+                           
+                           ),
+          
+          conditionalPanel(condition="input.Distribucion=='Gamma'",
+                           numericInput(inputId="mu_gamma",
+                                        label=HTML("Select &mu;:"),
+                                        min=0.01,
+                                        value=1.5,
+                                        step=0.01),
+                           
+                           numericInput(inputId="phi_gamma",
+                                        label=HTML("Select &phi;:"),
+                                        min=0.01,
+                                        value=0.3,
+                                        step=0.01),
+                           
+                           numericInput(inputId="min_gamma",
+                                        label="Select the minimun value for xlim",
+                                        value="0.01", step=1),
+                           numericInput(inputId="max_gamma",
+                                        label="Select the maximum value for xlim",
+                                        value="3", step=1)
+                           
+                           ),
+          
+          conditionalPanel(condition="input.Distribucion=='Inv. gaussian'",
+                           numericInput(inputId="mu_invgaus",
+                                        label=HTML("Select &mu;:"),
+                                        min=0.01,
+                                        value="1.5",
+                                        step=0.01),
+                           
+                           numericInput(inputId="phi_invgaus",
+                                        #label=HTML("Select &phi;:"),
+                                        label="Select φ",
+                                        min=0.1,
+                                        value="1.2",
+                                        step=0.1),
+                           
+                           numericInput(inputId="min_invgaus",
+                                        label="Select the minimun value for xlim",
+                                        value="0.01", step=1),
+                           numericInput(inputId="max_invgaus",
+                                        label="Select the maximum value for xlim",
+                                        value="3", step=1)
+                           
+                           ),
+          
+          conditionalPanel(condition="input.Distribucion=='Binomial'",
+                           numericInput(inputId="mu_binom",
+                                        label=HTML("Select &mu;:"),
+                                        min=0.01,
+                                        max=0.99,
+                                        value=0.3,
+                                        step=0.01),
+                           
+                           numericInput(inputId="m",
+                                        label=HTML("Select m the number of experiments"),
+                                        min=1,
+                                        value=7,
+                                        step=1)
+                           ),
+          
+          conditionalPanel(condition="input.Distribucion=='Poisson'",
+                           numericInput(inputId="mu_pois",
+                                        label=HTML("Select &mu;:"),
+                                        min=0.01,
+                                        value="2.7",
+                                        step=0.01),
+                           
+                           numericInput(inputId="max_pois",
+                                        label="Select the maximum value for xlim",
+                                        value="15", step=1)
+                           
+                           ),
+          
+          conditionalPanel(condition="input.Distribucion=='Negative binomial'",
+                           numericInput(inputId="mu_negbin",
+                                        label=HTML("Select &mu;:"),
+                                        min=0.01,
+                                        value="2.7",
+                                        step=0.01),
+                           
+                           numericInput(inputId="k",
+                                        label=HTML("Select k"),
+                                        min=0.01,
+                                        value="1.6",
+                                        step=0.01),
+                           
+                           numericInput(inputId="max_negbin",
+                                        label="Select the maximum value for xlim",
+                                        value="15", step=1)
+                           
+                           
+                           ),
+          
+          img(src="logo.png", height = 60, width = 140),
+          
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot", width = "400px")
+        )
+    )
+))
