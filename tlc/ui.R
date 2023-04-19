0091a79f418d905ab42a88cf7039927e568cde5d
fhernanb <fhernanb@unal.edu.co>
2023-04-19 09:53:22 -0500

    se agregó el taller

--- tlc/ui.R
+++ tlc/ui.R
@@ -130,8 +130,12 @@ fluidPage(
                   min=1, 
                   max=1000, step=NULL, animate=T),
   
+  br(),
+  
   downloadLink("download", 
                "Click here to get a workshop to interact with the app."),
+  
+  br(),
 
   p("App created by Semillero de R at Universidad Nacional de Colombia:"),
   tags$a(href="https://srunal.github.io", "https://srunal.github.io")
