a88f23ddf1bbf4ac18c19391db5c4b06b09d4a4a
fhernanb <fhernanb@unal.edu.co>
2024-03-04 21:05:39 -0500

    nueva app

--- Leccion_01_shiny_densidad_Beta/server.R
+++ Leccion_01_shiny_densidad_Beta/server.R
@@ -1,12 +1,11 @@
 library(shiny)
 
-# Define server logic required to draw a histogram
 function(input, output, session) {
 
     output$densi_Beta <- renderPlot({
 
         curve(dbeta(x, shape1=input$shape1, shape2=input$shape2),
-              lwd=3, col="tomato",
+              lwd=5, col="tomato",
               ylab="Densidad", 
               main="Densidad de una Beta")
 
--- Leccion_01_shiny_densidad_Beta/ui.R
+++ Leccion_01_shiny_densidad_Beta/ui.R
@@ -1,12 +1,8 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
     titlePanel("Distribución Beta"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput(inputId = "shape1",
@@ -14,6 +10,7 @@ fluidPage(
                         min = 0,
                         max = 10,
                         value = 2),
+            
             sliderInput(inputId = "shape2",
                         label = "Parametro shape 2:",
                         min = 0,
@@ -21,7 +18,6 @@ fluidPage(
                         value = 5)
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
             plotOutput("densi_Beta")
         )
